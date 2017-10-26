a9577f9f378dbd46f3fca9ec625346dff15ac2da
yang-zhang <zhangyangcolostate@gmail.com>
2017-10-25 23:08:40 -0400

    Update prework

--- prework.py
+++ prework.py
@@ -3,16 +3,19 @@
 
 # [Dog Breed Identification](https://www.kaggle.com/c/dog-breed-identification)
 
-# In[60]:
+# In[45]:
 
 from secrets import KAGGLE_USER, KAGGLE_PW
 import utils_ds
 
 import pandas as pd
+import numpy as np
 import os
 
+from IPython.display import Image
 
-# In[89]:
+
+# In[2]:
 
 competition_name = 'dog-breed-identification'
 
@@ -21,17 +24,17 @@ dir_data = '/opt/notebooks/data/' + competition_name
 
 # ### download
 
-# In[5]:
+# In[9]:
 
 get_ipython().system('mkdir -p $dir_data/raw ')
 
 
-# In[6]:
+# In[10]:
 
 get_ipython().magic('cd $dir_data/raw')
 
 
-# In[7]:
+# In[11]:
 
 get_ipython().system('kg config -g -u $KAGGLE_USER -p $KAGGLE_PW -c $competition_name')
 get_ipython().system('kg download')
@@ -39,22 +42,22 @@ get_ipython().system('kg download')
 
 # ### unzip
 
-# In[13]:
+# In[3]:
 
 get_ipython().system('mkdir $dir_data/preprocessed')
 
 
-# In[14]:
+# In[12]:
 
 get_ipython().magic('cp $dir_data/raw/*.zip $dir_data/preprocessed/')
 
 
-# In[ ]:
+# In[13]:
 
 get_ipython().system("unzip '$dir_data/preprocessed/*.zip' -d $dir_data/preprocessed")
 
 
-# In[16]:
+# In[14]:
 
 get_ipython().magic('ls $dir_data/preprocessed/train/ -l | wc -l')
 get_ipython().magic('ls $dir_data/preprocessed/test/ -l | wc -l')
@@ -65,19 +68,19 @@ get_ipython().magic('ls $dir_data/preprocessed/test/ -l | wc -l')
 rm $dir_data/preprocessed/*.zip
 
 
-# In[20]:
+# In[16]:
 
 ls $dir_data/preprocessed/
 
 
 # ### move samples to validation folder
 
-# In[30]:
+# In[17]:
 
 get_ipython().system('mkdir $dir_data/preprocessed/valid ')
 
 
-# In[32]:
+# In[18]:
 
 utils_ds.move_sample(
     dir_source=dir_data + '/preprocessed/train',
@@ -86,51 +89,51 @@ utils_ds.move_sample(
     n=2000)
 
 
-# In[33]:
+# In[19]:
 
 get_ipython().magic('ls $dir_data/preprocessed/valid/ -l | wc -l')
 
 
 # ### copy samples to sample folder
 
-# In[34]:
+# In[20]:
 
 get_ipython().system('mkdir $dir_data/sample')
 
 
-# In[36]:
+# In[21]:
 
 get_ipython().system('mkdir $dir_data/sample/train $dir_data/sample/test $dir_data/sample/valid')
 
 
-# In[37]:
+# In[22]:
 
 utils_ds.copy_sample(
     dir_source=dir_data + '/preprocessed/train',
     dir_destin=dir_data + '/sample/train',
     file_type='jpg',
-    n=200)
+    n=1200)
 
 
-# In[38]:
+# In[23]:
 
 utils_ds.copy_sample(
     dir_source=dir_data + '/preprocessed/test',
     dir_destin=dir_data + '/sample/test',
     file_type='jpg',
-    n=200)
+    n=1200)
 
 
-# In[39]:
+# In[24]:
 
 utils_ds.copy_sample(
     dir_source=dir_data + '/preprocessed/valid',
     dir_destin=dir_data + '/sample/valid',
     file_type='jpg',
-    n=50)
+    n=1000)
 
 
-# In[88]:
+# In[25]:
 
 get_ipython().magic('ls $dir_data/sample/train/ -l | wc -l')
 get_ipython().magic('ls $dir_data/sample/test/ -l | wc -l')
@@ -139,37 +142,37 @@ get_ipython().magic('ls $dir_data/sample/valid/ -l | wc -l')
 
 # ### make results dirs
 
-# In[41]:
+# In[26]:
 
 mkdir $dir_data/preprocessed/results
 
 
-# In[42]:
+# In[27]:
 
 mkdir $dir_data/sample/results
 
 
 # ### rearrange data into labeled folders
 
-# In[43]:
+# In[28]:
 
 labels = pd.read_csv(dir_data+'/preprocessed/labels.csv')
 
 
-# In[44]:
+# In[29]:
 
 breeds = set(labels.breed)
 
 
 # #### train
 
-# In[61]:
+# In[30]:
 
 for breed in breeds:
     os.mkdir(dir_data+'/preprocessed/train/'+breed)
 
 
-# In[81]:
+# In[31]:
 
 for row in labels.iterrows():
     id_=row[1]['id']
@@ -184,13 +187,13 @@ for row in labels.iterrows():
 
 # #### valid
 
-# In[82]:
+# In[70]:
 
 for breed in breeds:
     os.mkdir(dir_data+'/preprocessed/valid/'+breed)
 
 
-# In[83]:
+# In[71]:
 
 for row in labels.iterrows():
     id_=row[1]['id']
@@ -205,13 +208,13 @@ for row in labels.iterrows():
 
 # #### train in sample
 
-# In[84]:
+# In[34]:
 
 for breed in breeds:
     os.mkdir(dir_data+'/sample/train/'+breed)
 
 
-# In[85]:
+# In[35]:
 
 for row in labels.iterrows():
     id_=row[1]['id']
@@ -226,13 +229,13 @@ for row in labels.iterrows():
 
 # #### valid in sample
 
-# In[86]:
+# In[36]:
 
 for breed in breeds:
     os.mkdir(dir_data+'/sample/valid/'+breed)
 
 
-# In[87]:
+# In[37]:
 
 for row in labels.iterrows():
     id_=row[1]['id']
@@ -243,4 +246,20 @@ for row in labels.iterrows():
     # pic is in train folder
     except OSError: 
         pass
+
+
+# ### Verify
+
+# In[76]:
+
+breed = np.random.choice(list(breeds))
+imgs = os.listdir(dir_data+'/preprocessed/valid/'+breed)
+img = np.random.choice(imgs)
+print(breed)
+Image(dir_data+'/preprocessed/valid/%s/%s' % (breed, img))
+
+
+# In[ ]:
+
+
 

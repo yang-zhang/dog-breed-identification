c3414cb5339745b685d05e1dab8b884875dbba0b
yang-zhang <zhangyangcolostate@gmail.com>
2017-10-29 15:45:39 -0400

    Lambda preprocess

--- beluga_batch.py
+++ beluga_batch.py
@@ -1,247 +1,234 @@
 
 # coding: utf-8
 
-# In[46]:
+# In[123]:
 
-get_ipython().magic('matplotlib inline')
-import pandas as pd
-import numpy as np
 import math
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import os
+import datetime
 
-from IPython import display
-from IPython.display import Image
-
-from os import listdir
-from datetime import datetime
+import numpy as np
+import pandas as pd
 
 from keras.preprocessing import image
+from keras.layers import Input, Lambda
+from keras.models import Model
 
 from keras.applications import xception
 from keras.applications import inception_v3
-from keras.utils.np_utils import to_categorical
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss, accuracy_score
-from sklearn.preprocessing import LabelEncoder
 
 from secrets import KAGGLE_USER, KAGGLE_PW
 
 
-# ### Get batches
-
-# In[2]:
-
-seed = 2014
-batch_size = 32
-
-
-# In[3]:
+# In[26]:
 
 competition_name = 'dog-breed-identification'
 data_dir = '/opt/notebooks/data/' + competition_name + '/preprocessed'
 
 
-# In[4]:
+# In[27]:
 
 gen = image.ImageDataGenerator()
 
 
-# In[5]:
+# In[80]:
 
-batches = gen.flow_from_directory(data_dir+'/train', target_size=(299, 299), shuffle=False, batch_size=batch_size)
-batches_val = gen.flow_from_directory(data_dir+'/valid', target_size=(299, 299), shuffle=False, batch_size=batch_size)
+batch_size = 32
+target_size=(299, 299)
 
 
-# In[12]:
+# In[63]:
+
+def add_preprocess(base_model, preprocess_func, inputs_shape=(299, 299, 3)):
+    inputs = Input(shape=inputs_shape)
+    x = Lambda(preprocess_func)(inputs)
+    outputs = base_model(x)
+    model = Model(inputs, outputs)
+    return model
+
+
+# ### Xception
+
+# In[89]:
+
+batches = gen.flow_from_directory(data_dir+'/train', shuffle=False, target_size=target_size, batch_size=batch_size)
+batches_val = gen.flow_from_directory(data_dir+'/valid', shuffle=False, target_size=target_size, batch_size=batch_size)
+
+nb_batches = math.ceil(batches.n/batch_size)
+nb_batches_val = math.ceil(batches_val.n/batch_size)
 
 y_encode = batches.classes
 y_val_encode = batches_val.classes
 
 
-# In[6]:
+# In[64]:
 
-def preprocess_batches(batches):
-    while True:
-        try:
-            batch = batches.next()
-            imgs = batch[0]
-            imgs = np.apply_along_axis(xception.preprocess_input, 0, imgs)
-            yield batch
-        except StopIteration:
-            break
+base_model = xception.Xception(weights='imagenet', include_top=False, pooling='avg')
 
 
-# ### Extract Xception bottleneck features
+# In[65]:
 
-# In[7]:
-
-batches_preprocessed = preprocess_batches(batches)
-batches_val_preprocessed = preprocess_batches(batches_val)
+model_x = add_preprocess(base_model, xception.preprocess_input)
 
 
-# In[8]:
+# In[66]:
 
-xception_bottleneck = xception.Xception(weights='imagenet', include_top=False, pooling='avg')
-
-
-# In[9]:
-
-nb_batches = math.ceil(batches.n/batch_size)
-bf_x = xception_bottleneck.predict_generator(batches_preprocessed, 
-                                           steps=nb_batches,
-                                           verbose=1)
-
-nb_batches_val = math.ceil(batches_val.n/batch_size)
-bf_val_x = xception_bottleneck.predict_generator(batches_val_preprocessed, 
-                                           steps=nb_batches_val,
-                                           verbose=1)
+bf_x=model_x.predict_generator(batches, steps=nb_batches, verbose=1)
 
 
-# ### LogReg on Xception bottleneck features
+# In[67]:
 
-# In[13]:
+bf_val_x=model_x.predict_generator(batches_val, steps=nb_batches_val, verbose=1)
 
-logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=seed)
+
+# In[68]:
+
+logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
 logreg.fit(bf_x, y_encode)
+
+
+# In[69]:
 
 valid_probs = logreg.predict_proba(bf_val_x)
 valid_preds = logreg.predict(bf_val_x)
 
 
-# In[14]:
+# In[70]:
 
 log_loss(y_val_encode, valid_probs)
 
 
-# In[15]:
+# In[71]:
 
 accuracy_score(y_val_encode, valid_preds)
 
 
-# ### Extract Inception bottleneck features
+# ### Inception
 
-# In[27]:
+# In[90]:
 
-batches = gen.flow_from_directory(data_dir+'/train', target_size=(299, 299), shuffle=False)
-batches_val = gen.flow_from_directory(data_dir+'/valid', target_size=(299, 299), shuffle=False)
-batches_preprocessed = preprocess_batches(batches)
-batches_val_preprocessed = preprocess_batches(batches_val)
+batches = gen.flow_from_directory(data_dir+'/train', shuffle=False, target_size=target_size, batch_size=batch_size)
+batches_val = gen.flow_from_directory(data_dir+'/valid', shuffle=False, target_size=target_size, batch_size=batch_size)
 
+nb_batches = math.ceil(batches.n/batch_size)
+nb_batches_val = math.ceil(batches_val.n/batch_size)
 
-# In[ ]:
-
-inception_bottleneck = inception_v3.InceptionV3(weights='imagenet', include_top=False, pooling='avg')
-
-
-# In[ ]:
-
-bf_i = inception_bottleneck.predict_generator(batches_preprocessed, 
-                                           steps=nb_batches,
-                                           verbose=1)
-bf_val_i = inception_bottleneck.predict_generator(batches_val_preprocessed, 
-                                           steps=nb_batches_val,
-                                           verbose=1)
+y_encode = batches.classes
+y_val_encode = batches_val.classes
 
 
-# ### LogReg on Inception bottleneck features
+# In[81]:
 
-# In[ ]:
+base_model = inception_v3.InceptionV3(weights='imagenet', include_top=False, pooling='avg')
 
-logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=seed)
+
+# In[82]:
+
+model_i = add_preprocess(base_model, inception_v3.preprocess_input)
+
+
+# In[83]:
+
+bf_i = model_i.predict_generator(batches, steps=nb_batches, verbose=1)
+
+
+# In[84]:
+
+bf_val_i = model_i.predict_generator(batches_val, steps=nb_batches_val, verbose=1)
+
+
+# In[85]:
+
+logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
 logreg.fit(bf_i, y_encode)
 
 
-# In[ ]:
+# In[86]:
 
 valid_probs = logreg.predict_proba(bf_val_i)
 valid_preds = logreg.predict(bf_val_i)
 
 
-# In[ ]:
+# In[87]:
 
 log_loss(y_val_encode, valid_probs)
 
 
-# In[ ]:
+# In[88]:
 
 accuracy_score(y_val_encode, valid_preds)
 
 
 # ### LogReg on all bottleneck features
 
-# In[37]:
-
-bf_x_val = bf_val
-
-
-# In[39]:
+# In[95]:
 
 X = np.hstack([bf_x, bf_i])
-V = np.hstack([bf_x_val, bf_val_i])
-logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs', random_state=seed)
+V = np.hstack([bf_val_x, bf_val_i])
+logreg = LogisticRegression(multi_class='multinomial', solver='lbfgs')
 logreg.fit(X, y_encode)
 valid_probs = logreg.predict_proba(V)
 valid_preds = logreg.predict(V)
 
 
-# In[40]:
+# In[96]:
 
 log_loss(y_val_encode, valid_probs)
 
 
+# In[97]:
+
+accuracy_score(y_val_encode, valid_preds)
+
+
 # ### predict test data
 
-# In[220]:
+# In[101]:
 
-test_ids = [file.split('.')[0] for file in listdir(data_dir+'/test/unknown')]
-
-
-# In[221]:
-
-batches_test = gen.flow_from_directory(data_dir+'/test', target_size=(299, 299), 
-                                       shuffle=False)
+test_ids = [file.split('.')[0] for file in os.listdir(data_dir+'/test/unknown')]
 
 
-# In[228]:
+# In[102]:
 
 test_ids[:3]
 
 
-# In[230]:
+# In[103]:
 
+batches_test = gen.flow_from_directory(data_dir+'/test', shuffle=False, target_size=target_size, batch_size=batch_size)
 batches_test.filenames[:3]
 
 
-# In[98]:
+# In[104]:
 
+batches_test = gen.flow_from_directory(data_dir+'/test', shuffle=False, target_size=target_size, batch_size=batch_size)
 nb_batches_test = math.ceil(batches_test.n/batch_size)
 
 
-# In[ ]:
+# In[105]:
 
-batches_test = gen.flow_from_directory(data_dir+'/test', target_size=(299, 299), 
-                                       shuffle=False)
-batches_test_preprocessed = preprocess_batches(batches_test)
-
-bf_x_test = xception_bottleneck.predict_generator(batches_test_preprocessed, 
+bf_x_test = model_x.predict_generator(batches_test, 
                                            steps=nb_batches_test,
                                            verbose=1)
 
 
-# In[ ]:
+# In[106]:
 
-batches_test = gen.flow_from_directory(data_dir+'/test', target_size=(299, 299), 
-                                       shuffle=False)
-batches_test_preprocessed = preprocess_batches(batches_test)
+batches_test = gen.flow_from_directory(data_dir+'/test', shuffle=False, target_size=target_size, batch_size=batch_size)
+nb_batches_test = math.ceil(batches_test.n/batch_size)
 
-bf_i_test = inception_bottleneck.predict_generator(batches_test_preprocessed, 
+
+# In[107]:
+
+bf_i_test = model_i.predict_generator(batches_test, 
                                            steps=nb_batches_test,
                                            verbose=1)
 
 
-# In[174]:
+# In[108]:
 
 X_test = np.hstack([bf_x_test, bf_i_test])
 test_probs = logreg.predict_proba(X_test)
@@ -249,37 +236,41 @@ test_probs = logreg.predict_proba(X_test)
 
 # ### Make test submission file
 
-# In[202]:
+# In[111]:
 
 subm=pd.DataFrame(np.hstack([np.array(test_ids).reshape(-1, 1), test_probs]))
 
 
-# In[127]:
+# In[112]:
 
 labels = pd.read_csv(data_dir+'/labels.csv')
 
 
-# In[128]:
+# In[113]:
 
 cols = ['id']+sorted(labels.breed.unique())
 
 
-# In[129]:
+# In[118]:
 
 subm.columns = cols
-description = 'beluga_batch'
+description = 'beluga_batch_lambda_preprocess'
 submission_file_name = data_dir+'/results/%s_%s.csv' % (description,
-                                                        datetime.now().strftime('%Y-%m-%d-%H-%M')
+                                                        datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
                                                        )
 subm.to_csv(submission_file_name, index=False)
 
 
 # ### submit
 
-# In[216]:
+# In[124]:
 
 get_ipython().system('kg config -g -u $KAGGLE_USER -p $KAGGLE_PW -c $competition_name')
+
+
+# In[125]:
+
 get_ipython().system('kg submit $submission_file_name -u $KAGGLE_USER -p $KAGGLE_PW -m $description')
 
 
-# Your submission scored 0.30091
+# Your submission scored 
